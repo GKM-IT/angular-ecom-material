a0384b26a5b793600f6674ff1a90371b8f9679e6
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-05-22 15:40:20 +0530

    add pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { PhonePipe } from './phone.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    AdminComponent
+    AdminComponent,
+    PhonePipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
