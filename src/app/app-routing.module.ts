d1578f66275e0d86418ec7b0dfe9f04d32c5188c
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-05-22 15:10:07 +0530

    basic admin layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const homeChildRoutes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'add',
+    component: DashboardComponent
+  },
+  {
+    path: 'update/:id',
+    component: DashboardComponent
+  },
+  {
+    path: 'detail/:id',
+    component: DashboardComponent
+  }
+];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: homeChildRoutes,
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
