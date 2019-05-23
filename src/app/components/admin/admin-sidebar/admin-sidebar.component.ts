81d4da3d4469d31cd0b7e167b28b76e83e12cfd5
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-05-23 16:10:13 +0530

    dynamic sidebar

--- src/app/components/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/app/components/admin/admin-sidebar/admin-sidebar.component.ts
@@ -5,10 +5,45 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin-sidebar.component.html',
   styleUrls: ['./admin-sidebar.component.css']
 })
-export class AdminSidebarComponent implements OnInit {  
+export class AdminSidebarComponent implements OnInit {
+  public menuData = [];
   constructor() { }
 
   ngOnInit() {
+    this.menuData.push({
+      name: 'Dashboard',
+      link: '/',
+      icon: 'dashboard'
+    });
+
+    this.menuData.push({
+      name: 'User Module',
+      icon: 'account_circle',
+      chidren: [
+        {
+          name: 'All User Groups',
+          link: '/user-groups',
+        },
+        {
+          name: 'All Users',
+          link: '/users',
+        }
+      ]
+    });
+    this.menuData.push({
+      name: 'Employee Module',
+      icon: 'account_circle',
+      chidren: [
+        {
+          name: 'All Employee Groups',
+          link: '/user-groups',
+        },
+        {
+          name: 'All Employees',
+          link: '/users',
+        }
+      ]
+    });
   }
 
 }
