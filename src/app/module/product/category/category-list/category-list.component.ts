f0214ab92835e802a45c9cf311115684ba8aa0f0
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-06-23 13:44:16 +0530

    updates

--- src/app/module/product/category/category-list/category-list.component.ts
+++ src/app/module/product/category/category-list/category-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/providers/product/category.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Constant } from 'src/app/helper/constant';
+import { ConfirmDialogComponent } from 'src/app/components/common/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +14,126 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['select', 'name', 'updated_at', 'action'];
+  selection = new SelectionModel<any>(true, []);
+  dataSource: any[] = [];
+  filterData = {
+    length: 0,
+    pageSize: 10,
+    pageIndex: 0,
+    search: '',
+    sort_by: 'name',
+    sort_dir: 'asc'
+  };
+
+  pageSizeOptions;
+
+  constructor(
+    private masterService: CategoryService,
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    private router: Router,
+  ) {
+    const constant = new Constant();
+    this.pageSizeOptions = constant.pageSizeOptions;
+  }
 
   ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.masterService.list(this.filterData).subscribe(response => {
+      this.dataSource = response.data;
+      this.filterData.length = response.recordsFiltered;
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    this.filterData.search = filterValue.trim().toLowerCase();
+    this.filterData.pageIndex = 0;
+    this.getData();
+  }
+
+  changePage(event) {
+    this.filterData.pageIndex = event.pageIndex;
+    this.filterData.pageSize = event.pageSize;
+    this.getData();
+  }
+
+  sortData(event) {
+    this.filterData.sort_by = event.active;
+    this.filterData.sort_dir = event.direction;
+    this.filterData.pageIndex = 0;
+    this.getData();
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.forEach(row => this.selection.select(row));
+  }
+
+  checkboxLabel(row?: any): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+
+  deleteAll() {
+    if (this.selection.selected.length) {
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        width: '300px',
+        data: { title: 'Delete confirmation', content: `Are you sure want to delete selected ${this.selection.selected.length} records ?` }
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.masterService.deleteAll(this.selection.selected).subscribe(response => {
+            this.snackBar.open(response.message, 'X', {
+              duration: 2000,
+            });
+          });
+        }
+      });
+    } else {
+      this.snackBar.open('Please select rows', 'X', {
+        duration: 2000,
+      });
+    }
+  }
+
+  delete(row) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '300px',
+      data: { title: 'Delete confirmation', content: `Are you sure want to delete ${row.name} ?` }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.masterService.delete(row.id).subscribe(response => {
+          this.snackBar.open(response.message, 'X', {
+            duration: 2000,
+          });
+        });
+      }
+    });
+
+  }
+
+  edit(row) {
+    this.router.navigate(['/category/', row.id]);
+  }
+
+  add() {
+    this.router.navigate(['/category/', 'new']);
   }
 
 }
---
+++ src/app/providers/product/product-review.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@angular/core';
+import { ConfigService } from '../config/config.service';
+import { isString } from 'util';
+import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductReviewService {
+
+  public formData: FormData = new FormData();
+  private url;
+
+  constructor(public http: HttpClient, public configService: ConfigService) {
+
+  }
+
+  public list(data: any) {
+    this.formData = new FormData();
+
+    if (data.search && isString(data.search)) {
+      this.formData.append('search', data.search);
+    }
+    if (data.pageSize) {
+      this.formData.append('length', data.pageSize);
+    }
+    if (data.pageIndex) {
+      this.formData.append('start', data.pageIndex);
+    }
+    if (data.sort_by) {
+      this.formData.append('sort_by', data.sort_by);
+    }
+    if (data.sort_dir) {
+      this.formData.append('sort_dir', data.sort_dir);
+    }
+
+    this.url = `${environment.url}product/product_reviews`;
+    return this.http.post<any>(this.url, this.formData).pipe(
+      // retry(1), // retry a failed request up to 3 times
+      catchError(this.configService.handleError)
+    );
+  }
+
+  public detail(id: any) {
+    this.formData = new FormData();
+
+    this.url = `${environment.url}product/product_reviews/detail`;
+    this.formData.append('id', id);
+    return this.http.post<any>(this.url, this.formData).pipe(
+      // retry(1), // retry a failed request up to 3 times
+      catchError(this.configService.handleError)
+    );
+  }
+
+  public delete(id: any) {
+    this.url = `${environment.url}product/product_reviews/delete/${id}`;
+    return this.http.get<any>(this.url).pipe(
+      // retry(1), // retry a failed request up to 3 times
+      catchError(this.configService.handleError)
+    );
+  }
+
+
+  public deleteAll(data: any) {
+    const selected = data.map(row => (
+      row.id
+    ));
+    this.formData = new FormData();
+    this.formData.append('list', JSON.stringify(selected));
+    this.url = `${environment.url}product/product_reviews/delete_all`;
+    return this.http.post<any>(this.url, this.formData).pipe(
+      // retry(1), // retry a failed request up to 3 times
+      catchError(this.configService.handleError)
+    );
+  }
+
+  public save(data: any, id: any) {
+    this.formData = new FormData();
+    this.url = `${environment.url}product/product_reviews/save`;
+    if (id) {
+      this.formData.append('id', id);
+    }
+    this.formData.append('name', data.name);
+    return this.http.post<any>(this.url, this.formData).pipe(
+      // retry(1), // retry a failed request up to 3 times
+      catchError(this.configService.handleError)
+    );
+  }
+}
