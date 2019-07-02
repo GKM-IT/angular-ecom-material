ddded35c045eb991326b06fcb24e0f877d4e1b32
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-07-02 22:21:05 +0530

    name update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  name: 'Pro Erp',
+  name: 'Pro Admin',
   url: 'http://ecom.xeronesoft.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  name: 'Dev Erp',
+  name: 'Dev Admin',
   url: 'http://localhost/ci/ci-ecom/'
 };
 
